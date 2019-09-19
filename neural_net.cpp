eb951f45e922c47117346309821638c426da8ccd
tim <tim@wood.com>
2019-09-19 21:47:50 +0100

    numerically stable softmax

--- neural_net.cpp
+++ neural_net.cpp
@@ -21,9 +21,26 @@ void ActivationFunction(
     std::size_t size) {
 
   double totalE = 0.0;
+  std::unique_ptr<double[]> outputsE;
+
   if (type == ActivationType::Softmax) {
-    for (std::size_t i = 0u; i < size; ++i)
-      totalE += std::exp(outputs[i]);
+    // std::exp will reach inf when x > ~700. We can ensure numerical stability
+    // by subtracting the maximum output value from each x before we call
+    // std::exp. This is mathematically equivalent to vanilla softmax.
+
+    double max = -std::numeric_limits<double>::max();
+
+    for (std::size_t i = 0u; i < size; ++i) {
+      if (outputs[i] > max)
+        max = outputs[i];
+    }
+
+    outputsE = std::make_unique<double[]>(size);
+
+    for (std::size_t i = 0u; i < size; ++i) {
+      outputsE[i] = std::exp(outputs[i] - max);
+      totalE += outputsE[i];
+    }
   }
 
   for (std::size_t i = 0u; i < size; ++i) {
@@ -46,7 +63,7 @@ void ActivationFunction(
       outputs[i] =  activation >= 0.0 ? activation : activation*0.01;
       continue;
     case ActivationType::Softmax:
-      outputs[i] = std::exp(activation) / totalE;
+      outputs[i] = outputsE[i] / totalE;
       continue;
     }
   }
